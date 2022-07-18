2f55c405784bf0acd3863da47bfad13b779ddb68
fullstack300622 <108460728+fullstack300622@users.noreply.github.com>
2022-07-18 21:29:35 +0300

    Create ClassCode.java

---
+++ ClassCode.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        int x = 5;
+//        if (x > 10) {
+//            System.out.println("X higher than 10");
+//        } else if (x < 10) {
+//            System.out.println("X lower than 10");
+//        } else {
+//            System.out.println("Even");
+//        }
+
+
+//        Scanner s = new Scanner(System.in);
+//        System.out.println("Please enter your name");
+//        int num = s.nextInt();
+
+
+//        if (false && true && false || true) {
+//            System.out.println("True");
+//        }
+
+
+//        int num = s.nextInt();
+//
+//        // Operator "And"  &&
+//        // Print a number that is Higher than 10 And Lower then 30
+//
+//        // true + true = true
+//        // true + false = false
+//        // false + true = false
+//        // false + false = false
+//        if (num > 10 && num < 30 ) {
+//            System.out.println("Number is between 10 and 30");
+//        }
+
+
+        //Print a number if number is lower than 10 or number is higher than 30
+        //Operator OR  ||
+
+        //true + true = true
+        //true + false = true
+        //false + true = true
+        //false + false = false
+//        if (num < 10 || num > 30) {
+//            System.out.println("Number is higher than 30 or lower than 10");
+//        }
+
+
+//        int numOne = 40;
+//        int numTwo = 40;
+
+//        if (numOne >= numTwo) {
+//            System.out.println("Num one is Higher or even than number two");
+//            System.out.println(numOne);
+//        }else {
+//            System.out.println("number two is Higher");
+//            System.out.println(numTwo);
+//        }
+
+
+    }
+}
